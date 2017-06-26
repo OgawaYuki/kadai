183b62064ea7c93b218c658d33e6896882f3b452
DESKTOP-IO1SPCI\yuuki <s17037@tokyo.kosen-ac.jp>
2017-06-26 15:32:24 +0900

    プロコン（自由部門）課題１
    プログラミング本体

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyaku_ni_siyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string uttayatu = Console.ReadLine();
+                int mozikazu = 0;
+                string dasuyatu = "";
+                int u_kazu = uttayatu.Length;
+
+                while (mozikazu != u_kazu)
+                {
+                    char gyaku = uttayatu[mozikazu];
+                    dasuyatu = gyaku + dasuyatu;
+                    mozikazu = mozikazu + 1;
+
+                }
+                Console.WriteLine(dasuyatu);
+                if (uttayatu == "end")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
